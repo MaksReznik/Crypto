5df1b2127d45e353988a39e3712bdbabc128be99
Maksym Riznyk <mriznyk@hhaexchange.com>
2022-11-13 10:11:01 +0200

    Version 1.0  4 Ciphers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+enum Mode {
+  Caesar = 1,
+  Transposition = 2,
+  Binary = 3,
+  All = 4,
+}
+
+enum TranspositionMode {
+  ByWord = 1,
+  Full = 2,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Crypto';
+  resultValue: string = '';
+  mode: Mode = 1;
+  shiftValue: number = 1;
+  transpositionMode: TranspositionMode = 1;
+
+  constructor(public clipboard: Clipboard) {}
+
+  caesarCipher(inputValue: string, isReverseProcess: boolean) {
+    inputValue = inputValue.toUpperCase();
+    const shiftValue = isReverseProcess ? -this.shiftValue : +this.shiftValue;
+    this.resultValue = inputValue?.replace(/[A-Z]/g, (symbol) => {
+      const charCode = symbol.charCodeAt(0);
+      return String.fromCharCode(
+        charCode + shiftValue <= 90
+          ? charCode + shiftValue
+          : ((charCode + shiftValue) % 90) + 64
+      );
+    });
+  }
+
+  transpositionCipher(inputValue: string) {
+    this.resultValue =
+      this.transpositionMode === 1
+        ? inputValue
+            .split(' ')
+            .map((word) => word.split('').reverse().join(''))
+            .join(' ')
+        : inputValue.split('').reverse().join('');
+  }
+
+  toBinaryCipher(inputValue: string, isReverseProcess: boolean) {
+    this.resultValue = isReverseProcess
+      ? inputValue
+          .split(' ')
+          .map((code) => String.fromCharCode(parseInt(code, 2)))
+          .join('')
+      : inputValue
+          .split('')
+          .map((symbol) => symbol.charCodeAt(0).toString(2))
+          .join(' ');
+  }
+
+  encrypt(inputValue: string, isReverseProcess = false) {
+    switch (this.mode) {
+      case 1:
+        this.caesarCipher(inputValue, isReverseProcess);
+        break;
+      case 2:
+        this.transpositionCipher(inputValue);
+        break;
+      case 3:
+        this.toBinaryCipher(inputValue, isReverseProcess);
+        break;
+      case 4:
+        if (!isReverseProcess) {
+          this.caesarCipher(inputValue, isReverseProcess);
+          setTimeout(() => {
+            this.transpositionCipher(this.resultValue);
+            setTimeout(() => {
+              this.toBinaryCipher(this.resultValue, isReverseProcess);
+            }, 1000);
+          }, 1000);
+        } else {
+          this.toBinaryCipher(inputValue, isReverseProcess);
+          setTimeout(() => {
+            this.transpositionCipher(this.resultValue);
+            setTimeout(() => {
+              this.caesarCipher(this.resultValue, isReverseProcess);
+            }, 1000);
+          }, 1000);
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  copyToClipboard(value: string) {
+    const pending = this.clipboard.beginCopy(value);
+    let remainingAttempts = 3;
+    const attempt = () => {
+      const result = pending.copy();
+      if (!result && --remainingAttempts) {
+        setTimeout(attempt);
+      } else {
+        pending.destroy();
+      }
+    };
+    attempt();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
+const materialModules = [MatInputModule, MatButtonModule];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ...materialModules,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ClipboardModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
